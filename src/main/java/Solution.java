e62ea8f17b785cf6c49796dc3498743d17041c41
LiquidPlummer <kyle.plummer@revature.com>
2023-03-21 13:02:04 -0400

    solution

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,7 +1,43 @@
 class Solution {
-    public IntegerList mergeTwoLists(IntegerList list1, IntegerList list2) {
-        //Write your code here...
+    /*
+    This solution
+     */
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null) {
+            return list2;
+        } else if (list2 == null) {
+            return list1;
+        }
 
-        return new IntegerList();
+        ListNode mergedList = new ListNode();
+        if(list1.val < list2.val) {
+            mergedList.val = list1.val;
+            list1 = list1.next;
+        } else {
+            mergedList.val = list2.val;
+            list2 = list2.next;
+        }
+
+        ListNode cursor = mergedList;
+        while(list1 != null || list2 != null) {
+            if(list1 == null) {
+                cursor.next = list2;
+                return mergedList;
+            } else if(list2 == null) {
+                cursor.next = list1;
+                return mergedList;
+            } else if(list1.val < list2.val) {
+                cursor.next = list1;
+                cursor = cursor.next;
+                list1 = list1.next;
+            } else {
+                cursor.next = list2;
+                cursor = cursor.next;
+                list2 = list2.next;
+            }
+        }
+
+
+        return mergedList;
     }
 }
